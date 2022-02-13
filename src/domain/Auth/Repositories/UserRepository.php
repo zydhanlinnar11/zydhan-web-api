d4593067b73774d4182365915a4ec0819bb6f883
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-13 23:51:23 +0700

    feat(auth): add findById to UserRepository interface

--- src/domain/Auth/Repositories/UserRepository.php
+++ src/domain/Auth/Repositories/UserRepository.php
@@ -3,8 +3,10 @@
 namespace Domain\Auth\Repositories;
 
 use Domain\Auth\Models\Entity\User;
+use Domain\Auth\Models\Value\UserId;
 
 interface UserRepository
 {
+    public function findById(UserId $user): ?User;
     public function create(User $user): User;
 }
