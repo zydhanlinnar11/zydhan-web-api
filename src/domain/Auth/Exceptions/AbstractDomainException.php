0e5636951cc689a0dc6ee1986334aa8e52f6eda0
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-15 05:41:12 +0700

    feat(auth): add AbstractDomainException to generalize domain exception

--- src/domain/Auth/Exceptions/AbstractDomainException.php
+++ src/domain/Auth/Exceptions/AbstractDomainException.php
@@ -2,18 +2,16 @@
 
 namespace Domain\Auth\Exceptions;
 
-class EmailAlreadyExistException extends AbstractDomainException
+use Exception;
+
+abstract class AbstractDomainException extends Exception
 {
     public function __construct(
-        string $message = "",
+        string $message,
         int $code = 0,
         \Throwable|null $previous = null
     )
     {
-        if (!$message) {
-            $message = "email_already_exist";
-        }
-
         parent::__construct($message, $code, $previous);
     }
 }
