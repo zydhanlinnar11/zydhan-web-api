18cd0789e10f2d7aa80d96a67241a72c47866d41
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-28 19:56:06 +0700

    feat(blog): change return type of findallbypost

--- src/Modules/Blog/Domain/Repositories/CommentRepositoryInterface.php
+++ src/Modules/Blog/Domain/Repositories/CommentRepositoryInterface.php
@@ -9,6 +9,12 @@ use Modules\Blog\Domain\Models\Value\PostId;
 interface CommentRepositoryInterface
 {
     public function findById(CommentId $commentId): ?Comment;
-    public function findAllByPostId(PostId $postId): ?Comment;
+
+    /**
+     * Get all comments by post.
+     * @param \Modules\Blog\Domain\Models\Value\PostId $postId
+     * @return \Modules\Blog\Domain\Models\Entity\Comment[]
+     */
+    public function findAllByPostId(PostId $postId);
     public function save(Comment $comment): bool;
 }
