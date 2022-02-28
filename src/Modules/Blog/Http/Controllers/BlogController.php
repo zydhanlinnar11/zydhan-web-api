5a3e34f26bb6bdc5006dc6e0a2910c05816379d2
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-28 10:42:07 +0700

    feat(blog): implement controller method and resource for home page post list

--- src/Modules/Blog/Http/Controllers/BlogController.php
+++ src/Modules/Blog/Http/Controllers/BlogController.php
@@ -2,19 +2,38 @@
 
 namespace Modules\Blog\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-
+use Modules\Blog\Domain\Models\Value\PostVisibility\PostVisibility;
+use Modules\Blog\Domain\Repositories\CommentRepositoryInterface;
+use Modules\Blog\Domain\Repositories\PostRepositoryInterface;
+use Modules\Blog\Transformers\HomePagePostsResource;
 class BlogController extends Controller
 {
+    public function __construct(
+        private PostRepositoryInterface $postRepository,
+        private CommentRepositoryInterface $commentRepository,
+    ) { }
+
+
     /**
      * Display a listing of the resource.
+     * @param Request $request
      * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        // 
+        try {
+            $posts = $this->postRepository->findByVisibilities([PostVisibility::VISIBLE]);
+            $data = (new HomePagePostsResource($posts))->toArray($request);
+
+            return response()->json(['status' => 'success', 'data' => $data]);
+        } catch (\Exception $e) {
+            if(env('APP_DEBUG') == 'true') {
+                return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
+            }
+            return response()->json(['status' => 'error', 'message' => 'Internal server error']);
+        } 
     }
 
     /**
