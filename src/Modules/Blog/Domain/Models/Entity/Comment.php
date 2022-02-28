89534a39127cf09bed02ca2022aca38969bb69d7
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2022-02-28 19:55:46 +0700

    feat(blog): add timestamp to comment model

--- src/Modules/Blog/Domain/Models/Entity/Comment.php
+++ src/Modules/Blog/Domain/Models/Entity/Comment.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Blog\Domain\Models\Entity;
 
+use DateTime;
 use Modules\Auth\Domain\Models\Value\UserId;
 use Modules\Blog\Domain\Models\Value\CommentId;
 use Modules\Blog\Domain\Models\Value\PostId;
@@ -12,7 +13,9 @@ class Comment
         private CommentId $id,
         private UserId $userId,
         private PostId $postId,
-        private string $comment
+        private string $comment,
+        private DateTime $createdAt,
+        private DateTime $updatedAt,
     ) { }
 
     public function getId(): CommentId
@@ -33,5 +36,15 @@ class Comment
     public function getComment(): string
     {
         return $this->comment;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
     }
 }
